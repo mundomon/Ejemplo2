310cc5827e0f3f41b41c7e3345b9e0e787a2f0c2
dedam <dedam@pia-69-8.uab.es>
2016-11-29 19:42:04 +0100

    Initial Commit

---
+++ Ejemplo2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ejemplo2
+//
+//  Created by dedam on 29/11/16.
+//  Copyright © 2016 dedam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
